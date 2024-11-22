4ba5487fadc64e82b3d5c9bf32fb1e5bc1dc36d3
Sarai <saraimontesa@gmail.com>
2024-11-22 20:29:57 +0100

    Subiendo borrador inicial

---
+++ libft/ft_atoi.c
@@ -0,0 +1,46 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_atoi.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sarmonte <sarmonte@student.42urduliz.co    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/05/08 11:53:08 by gcollet           #+#    #+#             */
+/*   Updated: 2024/11/22 19:19:29 by sarmonte         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+/* La fonction atoi() convertit le début de la chaîne pointée par nptr en 
+entier de type int . */
+/* VALEUR RENVOYÉE : Le résultat de la conversion.   */
+
+#include "libft.h"
+
+int	ft_atoi(const char *nptr)
+{
+	long	num;
+	int		sign;
+	int		i;
+
+	i = 0;
+	sign = 1;
+	num = 0;
+	while ((nptr[i] >= '\t' && nptr[i] <= '\r') || nptr[i] == ' ')
+		nptr++;
+	if (nptr[i] == '-')
+		sign = sign * -1;
+	if (nptr[i] == '+' || nptr[i] == '-')
+		nptr++;
+	while (nptr[i] >= '0' && nptr[i] <= '9')
+	{
+		num = (num * 10) + nptr[i] - '0';
+		nptr++;
+		if (num * sign > 2147483647)
+			return (-1);
+		if (num * sign < -2147483648)
+			return (0);
+	}
+	return (num * sign);
+}
+
+
